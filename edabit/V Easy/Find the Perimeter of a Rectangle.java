641c4be400e08a9d5b9b0f8e7ac516d0d3684156
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-19 12:10:32 +0200

    update edabit v easy challenges

---
+++ edabit/V Easy/Find the Perimeter of a Rectangle.java
@@ -0,0 +1,52 @@
+
+
+/***********************************************************************************************
+                                                                                               *                         
+Find the Perimeter of a Rectangle                                                              * 
+Create a function that takes length and width and finds the perimeter of a rectangle.          *  
+                                                                                               *                 
+Examples                                                                                       *             
+findPerimeter(6, 7) ➞ 26                                                                      *                                                                                             *                     
+                                                                                               *                                         
+findPerimeter(20, 10) ➞ 60                                                                    *                      
+                                                                                               *                 
+findPerimeter(2, 9) ➞ 22                                                                      *                                             
+Notes                                                                                          *                                     
+Don't forget to return the result.                                                             *     
+If you're stuck, find help in the Resources tab.                                               *     
+If you're really stuck, find solutions in the Solutions tab.                                   *         
+                                                                                               *                     
+************************************************************************************************/
+
+
+
+
+
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+
+
+
+public class Challenge {
+	public static int findPerimeter(int length, int width) {
+		return 2 * length + 2 * width;
+  }
+}
+
+
+
+
+
+public class Challenge {
+	public static int findPerimeter(int length, int width) {
+		int perimeter = 2 * length + 2 * width;
+		return perimeter;
+  }
+}
