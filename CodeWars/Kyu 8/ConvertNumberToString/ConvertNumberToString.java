40f7a4feef8001da066db4e73a8263b02f2765b9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-21 10:18:59 +0200

    update solutions to CodeWars Kyu8

---
+++ CodeWars/Kyu 8/ConvertNumberToString/ConvertNumberToString.java
@@ -0,0 +1,23 @@
+
+
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+Kata.numberToString(123); // returns "123";
+Kata.numberToString(999); // returns "999";
+ */
+
+
+ //#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+class ConvertNumberToString {
+
+    static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
---
+++ CodeWars/Kyu 8/ConvertNumberToString/ConvertNumberToStringTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ConvertNumberToStringTest {
+
+    @Test
+    void tests() {
+        assertEquals("67", ConvertNumberToString.numberToString(67));
+        assertEquals("123", ConvertNumberToString.numberToString(123));
+        assertEquals("999", ConvertNumberToString.numberToString(999));
+    }
+
+}
