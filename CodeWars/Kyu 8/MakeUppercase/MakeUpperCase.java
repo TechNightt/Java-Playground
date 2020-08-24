78facd4be6f97ddbedbb32cbc8ae7616b9970d6c
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-24 10:27:39 +0200

    add solutions to CodeWars Kata kyu8

---
+++ CodeWars/Kyu 8/MakeUppercase/MakeUpperCase.java
@@ -0,0 +1,24 @@
+package _8kyu._8kyu_makeuppercase;
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+
+
+
+
+/*
+Write function makeUpperCase.
+ */
+
+class MakeUpperCase {
+
+    static String makeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
