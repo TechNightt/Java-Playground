912b4b71734fc02066791b473ea075fbbb74b7d8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-04 16:22:47 +0200

    add solution to LeetCode: q876_middle_of_the_linked_list

---
+++ LeetCode/Easy/q876_middle_of_the_linked_list/solution1/ListNode.java
@@ -0,0 +1,18 @@
+package com.leet_code.easy.q876_middle_of_the_linked_list.solution1;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
---
+++ LeetCode/Easy/q876_middle_of_the_linked_list/solution1/Solution.java
@@ -0,0 +1,57 @@
+// Source : https://leetcode.com/problems/middle-of-the-linked-list/
+
+
+/***************************************************************************************************
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list. *
+ *                                                                                                 *
+ * If there are two middle nodes, return the second middle node.                                   *
+ *                                                                                                 *
+ *                                                                                                 *
+ *                                                                                                 *
+ * Example 1:                                                                                      *
+ *                                                                                                 *
+ * Input: [1,2,3,4,5]                                                                              *
+ * Output: Node 3 from this list (Serialization: [3,4,5])                                          *
+ * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).            *
+ * Note that we returned a ListNode object ans, such that:                                         *
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.            *
+ * Example 2:                                                                                      *
+ *                                                                                                 *
+ * Input: [1,2,3,4,5,6]                                                                            *
+ * Output: Node 4 from this list (Serialization: [4,5,6])                                          *
+ * Since the list has two middle nodes with values 3 and 4, we return the second one.              *
+ *                                                                                                 *
+ *                                                                                                 *
+ * Note:                                                                                           *
+ *                                                                                                 *
+ * The number of nodes in the given list will be between 1 and 100.                                *
+ **************************************************************************************************/
+
+ package com.leet_code.easy.q876_middle_of_the_linked_list.solution1;
+
+
+/***************************************************************************************************
+ * Thoughts on implementation:                                                                     *
+ * I go through the linked list and count then number of nodes.                                    *
+ * Then I go through the linked list again but this time only half the way.                        *
+ * This solution is faster than 100.00%                                                            *
+ * Time complexity is O(n)                                                                         *
+ * ************************************************************************************************/
+
+ class Solution {
+	public ListNode middleNode(ListNode head) {
+	        ListNode ptr = head;
+	        int count = 0;
+	        while(head != null) {
+	            ++count;
+	            head = head.next;
+	        }
+	        count /= 2;
+	        head = ptr;
+	        while(count > 0) {
+	            --count;
+	            head = head.next;
+	        }
+	        return head;
+	    }
+}
