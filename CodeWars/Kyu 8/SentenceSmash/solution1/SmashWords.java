12ee2b602d7fad8af7cdbfdc0f682b5a6703e1ad
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-04 16:26:43 +0200

    added solutiont to CodeWars Kyu8 'SmashWords'

---
+++ CodeWars/Kyu 8/SentenceSmash/solution1/SmashWords.java
@@ -0,0 +1,27 @@
+package com.code_wars.kyu8.sentence_smash.solution1;
+
+/**
+ * Write a method smash that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!<br><br>
+ * Example
+ * var words = ['hello', 'world', 'this', 'is', 'great'];
+ * smash(words); // returns "hello world this is great"
+ * Assumptions
+ * You can assume that you are only given words.
+ * You cannot assume the size of the array.
+ * You can assume that you will always get an array.
+ * What We're Testing
+ * We're testing basic loops and string manipulation. This is for beginners who are just learning loops and string manipulation.<br><br>
+ * Disclaimer
+ * This is for beginners so we want to test basic loops and string manipulation. Advanced users should easily be able to do this in one line.
+**/
+
+public class SmashWords {
+	
+	// ****************************************************
+	// * This method is used to smash all words together. *
+	// ****************************************************
+	public static String smash(String... words) {
+		return String.join(" ", words);
+	}
+	
+}
