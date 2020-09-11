98a118d539e933c59c7895fdb2f4f9a81f8f34c5
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-11 20:04:12 +0200

    added solutions to edabit/ easy 'Double Letters'

---
+++ edabit (+500 challenges)/Easy/Double Letters.java
@@ -0,0 +1,53 @@
+/*********************************************************************************************************************
+ 
+Double Letters
+Create a function that takes a word and returns true if the word has two consecutive identical letters.
+
+Examples
+doubleLetters("loop") ➞ true
+
+doubleLetters("yummy") ➞ true
+
+doubleLetters("orange") ➞ false
+
+doubleLetters("munchkin") ➞ false
+
+**********************************************************************************************************************/
+
+
+
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTION 1   
+
+
+public class Challenge {
+	public static boolean doubleLetters(String word) {
+		for (int i=0;i<word.length()-1;i++)
+			if (word.charAt(i)==word.charAt(i+1))
+				return true;
+		return false;
+	}
+}
+
+
+
+//#############################################################
+//#                        MY SOLUTION 2   
+
+
+public class Challenge {
+    public static boolean doubleLetters(String word) {
+        for (int i = 0; i < word.length() - 1; ++i) {
+            if (word.charAt(i) == word.charAt(i+1)) return true;
+        }
+        return false;
+    }
+}
