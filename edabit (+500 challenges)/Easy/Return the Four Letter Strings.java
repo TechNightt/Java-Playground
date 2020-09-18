5c9cf7509c2a19d6105da09abe0ede3b69b86e81
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-18 17:00:08 +0200

    added solutions to edabit/ easy 'Return the Four Letter Strings'

--- edabit (+500 challenges)/Easy/Return the Four Letter Strings.java
+++ edabit (+500 challenges)/Easy/Return the Four Letter Strings.java
@@ -24,9 +24,12 @@ You can expect valid strings for all test cases.
 //#############################################################
 //#                        MY SOLUTION 1   
 
-
-
-
+import java.util.*;
+public class Program {
+    public static String[] isFourLetters(String[] strs) {
+        return Arrays.stream(strs).filter(s -> s.length() == 4).toArray(String[]::new);
+    }
+}
 
 
 //#############################################################
@@ -34,14 +37,60 @@ You can expect valid strings for all test cases.
 
 
 
+import java.util.*;
+
+public class Program {
+    public static String[] isFourLetters(String[] s) {
+      ArrayList<String> list = new ArrayList();
+      
+      for(String i : s){
+        if(i.length() == 4){
+          list.add(i);
+        }
+      }
+      
+      String[] result = list.toArray(new String[list.size()]);
+      return result;
+    }
+}
+
 
 
 //#############################################################
 //#                        MY SOLUTION 3   
 
 
-
+public class Program {
+    public static String[] isFourLetters(String[] s) {
+      int count = 0;
+      for(int i = 0; i < s.length; i ++){
+        String tmp = s[i];
+        if(tmp.length()==4){
+          count++;
+        }
+      }
+      String[] words = new String[count];
+      int j = 0;
+      for(int i = 0; i < s.length; i ++){
+        String tmp = s[i];
+        if(tmp.length()==4){
+          words[j] = tmp;
+          j++;
+        }
+      }
+      return words;
+    }
+}
 
 
 //#############################################################
 //#                        MY SOLUTION 4
+
+
+import java.util.stream.Stream;
+
+public class Program {
+    public static String[] isFourLetters(String[] s) {
+      return Stream.of(s).filter(str -> str.length() == 4).toArray(String[]::new);
+    }
+}
