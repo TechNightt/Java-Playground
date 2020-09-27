cb1915c6210b79b828623db84039195151055bef
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-27 12:47:41 +0200

    added solutions to edabit/ easy 'Char-to-ASCII'

---
+++ edabit (+500 challenges)/Easy/Char-to-ASCII.java
@@ -0,0 +1,48 @@
+/*********************************************************************************************************************
+ Char-to-ASCII
+Create a function that returns the ASCII value of the passed in character.
+
+Examples
+ctoa('A') ➞ 65
+
+ctoa('m') ➞ 109
+
+ctoa('[') ➞ 91
+
+ctoa('\') ➞ 92
+
+Notes
+
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+**********************************************************************************************************************/
+
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTION 1   
+
+
+
+public class Challenge {
+	public static int ctoa(char c) {
+		return c;
+	}
+}
+
+
+//#############################################################
+//#                        MY SOLUTION 2   
+
+
+
+public class Challenge {
+	public static int ctoa(char c) {
+		return (int)c;
+	}
+}
+
