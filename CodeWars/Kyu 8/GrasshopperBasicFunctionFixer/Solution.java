d14e6d093212b4b99168ea9567f7753a52acc546
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-10-03 14:29:15 +0200

    added solution to CodeWars Kyu8 'GrasshopperBasicFunctionFixer'

---
+++ CodeWars/Kyu 8/GrasshopperBasicFunctionFixer/Solution.java
@@ -0,0 +1,23 @@
+package com.code_wars.kyu8.grasshopper__basic_function_fixer.solution1;
+
+/**
+ * 
+ * Kata's Name 
+ * Grasshopper - Basic Function Fixer<br><br>
+ * = Description 
+ * Fix the function
+ * I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.<br><br>
+ * Can you help me fix the function?
+ * 
+**/
+
+public class Solution {
+	
+	// *********************************************************************
+	// * This method returns number that is received by the method plus 5. *
+    // *********************************************************************  	
+	public static int addFive(int num) {
+		return num + 5;
+	}
+  
+}
