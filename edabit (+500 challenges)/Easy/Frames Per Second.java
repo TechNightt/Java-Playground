f045d3e41b1c62a29bf6d5cfe732228d5de92667
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-10-05 14:37:49 +0200

    added solutions to edabit/ easy 'Frames Per Second'

---
+++ edabit (+500 challenges)/Easy/Frames Per Second.java
@@ -0,0 +1,32 @@
+/*********************************************************************************************************************
+ Frames Per Second
+Create a method that returns the number of frames shown in a given number of minutes for a certain FPS.
+
+Examples
+frames(1, 1) ➞ 60
+
+frames(10, 1) ➞ 600
+
+frames(10, 25) ➞ 15000
+
+Notes
+FPS stands for "frames per second" and it's the number of frames a computer screen shows every second.
+**********************************************************************************************************************/
+
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTION 1   
+//#############################################################
+
+
+public class Challenge {
+	public static int frames(int min, int fps) {
+		return min * 60 * fps;
+	}
+}
+
+
