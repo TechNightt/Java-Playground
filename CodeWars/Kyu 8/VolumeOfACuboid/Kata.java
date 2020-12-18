a8ccd08550d4f38bc5382f6bf07f6c4a971a0029
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-12-18 14:54:13 +0100

    added solution to CodeWars Kyu8 'VolumeOfACuboid'

---
+++ CodeWars/Kyu 8/VolumeOfACuboid/Kata.java
@@ -0,0 +1,20 @@
+package com.code_wars.kyu8.volume_of_a_cuboid.solution1;
+
+/**
+ * = Kata's Name =<br>
+ * Volume of a Cuboid<br><br>
+ * = Description = <br><br>
+ * Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+ * 
+**/
+
+public class Kata {
+	
+	// *****************************************************
+	// * This method is used to return volume of a cuboid. *
+	// *****************************************************
+	public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+		return length * width * height;
+	}
+  
+}
