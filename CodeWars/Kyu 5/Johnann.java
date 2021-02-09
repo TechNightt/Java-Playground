a2b3ed78484b64350638e2d00737577d53e926f2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2021-02-09 21:09:01 +0100

    added solution to CodeWars Kyu5  'Johnann'

---
+++ CodeWars/Kyu 5/Johnann.java
@@ -0,0 +1,47 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+    Condition: https://www.codewars.com/kata/57591ef494aba64d14000526
+ */
+
+public class Johnann {
+    private static List<Long> katasJohn = new ArrayList<Long>(Collections.singletonList(0L));
+    private static List<Long> katasAnn = new ArrayList<Long>(Collections.singletonList(1L));
+
+    public static List<Long> john(long n) {
+        if (n <= katasJohn.size()) {
+            return katasJohn.stream().limit(n).collect(Collectors.toList());
+        }
+        fill(n);
+        return katasJohn;
+    }
+
+    public static List<Long> ann(long n) {
+        if (n <= katasJohn.size()) {
+            return katasAnn.stream().limit(n).collect(Collectors.toList());
+        }
+        fill(n);
+        return katasAnn;
+    }
+
+    public static long sumJohn(long n) {
+        return john(n).stream().mapToLong(Long::longValue).sum();
+    }
+
+    public static long sumAnn(long n) {
+        return ann(n).stream().mapToLong(Long::longValue).sum();
+    }
+
+    private static void fill(long n) {
+        for (int i = katasJohn.size(); i < n; i++) {
+            katasJohn.add(i - katasAnn.get(Math.toIntExact(katasJohn.get(i - 1))));
+            katasAnn.add(i - katasJohn.get(Math.toIntExact(katasAnn.get(i - 1))));
+        }
+    }
+}
